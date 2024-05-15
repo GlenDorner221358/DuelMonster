9ea2a32010ae836dc06cbd0abe1a63b92325ef3a
GlenDorner221358 <221358@virtualwindow.co.za>
2024-05-15 16:18:53 +0200

    Login&Register Styling
    Finished the styling for the login and register screens + navigation

--- screens/registerScreen.jsx
+++ screens/registerScreen.jsx
@@ -1,31 +1,127 @@
-import { StyleSheet, View, Text, Button } from 'react-native'
+import { StyleSheet, View, Image, Text, TextInput, Button, Pressable } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 function RegisterScreen( {navigation} ) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Register Screen</Text>
-      <Button
-        title="Login"
-        onPress={() => navigation.navigate('login')}
+
+      <Image 
+        style={styles.logo}
+        source={require('../assets/logoTitle.png')}
       />
+
+      <View style={styles.loginPanel}>
+        <Text style={styles.title}>Register</Text>
+
+        <View style={styles.Lawrence}>
+          <Text style={styles.Clarence}> Name: </Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Seto"
+          />
+        </View>
+
+        <View style={styles.Lawrence}>
+          <Text style={styles.Clarence}> Email: </Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Kaiba@KaibaCorp.co.za"
+          />
+        </View>
+
+        <View style={styles.Lawrence}>
+          <Text style={styles.Clarence}> Password: </Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="BlueEyesWhiteDragon"
+          />
+        </View>
+
+        <View style={styles.Bertram}>
+          <Pressable style={{alignItems: "center"}} onPress={() => navigation.navigate('register')}>
+            <Text style={{color: "white", fontSize: 21}}> Create Account </Text>
+          </Pressable>
+        </View>
+      </View>
+
+      <View>
+        <Pressable style={styles.RegisterLink} onPress={() => navigation.navigate('login')}>
+            <Text style={{color: "white", fontSize: 15}}> Already have an account? </Text>
+            <Text style={{color: "#D1AC00", fontSize: 15}}> Log in here </Text>
+        </Pressable>
+      </View>
+
     </View>
   )
 }
 
 export default RegisterScreen
 
-
 const styles = StyleSheet.create({
   container: {
       flex: 1,
-      justifyContent: "center",
+      // justifyContent: "center",
       alignItems: "center",
-      backgroundColor: "#151718"
+      backgroundColor: "#01172f",
+      padding: 25,
+      paddingBottom: 60,
+      paddingTop: 30
   },
   title: {
-      fontSize: 20,
+      fontSize: 30,
       fontWeight: 'bold',
-      color: "#ECEDEE"
+      color: "#01172f",
+  },
+  loginPanel: {
+    backgroundColor: "#DBE4EE",
+    flex: 1,
+    padding: 10,
+    width: 275,
+    borderRadius: 9,
+    alignItems: "center",
+  },
+  textInput: {
+    width: "100%",
+    backgroundColor: "white",
+    padding: 5,
+    height: 40,
+    borderRadius: 5,
+    borderTopLeftRadius: 0,
+    borderColor: "gray",
+    borderWidth: 1
+  },
+  Bertram: {
+    marginTop: 20,
+    width: "100%",
+    backgroundColor: "#D1AC00",
+    borderRadius: 5,
+    padding: 5
+  },
+  Lawrence: {
+    flexDirection: "column",
+    marginBottom: 10,
+    marginTop: 20,
+    width: "100%",
+  },
+  Clarence: {
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderBottomWidth: 0,
+    width: 90,
+    backgroundColor: "aliceblue",
+    fontWeight: "500"
+  },
+  RegisterLink: {
+    marginTop: 10,
+    alignItems: 'center'
+  },
+  logo: {
+    width: 220,
+    height: 70,
+    borderRadius: 5,
+    marginBottom: 20
   }
 })
